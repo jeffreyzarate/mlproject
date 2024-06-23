65ec603888875653c0d4a55175ee94f61fbaa682
jeffreyzarate <jeffreyzarate1@gmail.com>
2024-06-22 22:02:10 -0400

    Fixing project structure and sample json

--- src/pipeline/predict_request.py
+++ src/pipeline/predict_request.py
@@ -21,21 +21,23 @@ class PredictRequest(BaseModel):
 
     class Config:
         json_schema_extra = {
-            "production_volume": 202,
-            "production_cost": 13175.403783,
-            "supplier_quantity": 86.648534,
-            "delivery_delay": 1,
-            "defect_rate": 3.121492,
-            "quality_score": 63.463494,
-            "maintenance_hours": 9,
-            "downtime_percentage": 0.052343,
-            "inventory_turnover": 8.630515,
-            "stock_out_rate": 0.081322,
-            "worker_productivity": 85.042379,
-            "safety_incidents": 0,
-            "energy_consumption": 2419.616785,
-            "energy_efficiency": 0.468947,
-            "additive_process_time": 5.551639,
-            "additive_material_cost": 236.439301
+            "example": {
+                "production_volume": 202,
+                "production_cost": 13175.403783,
+                "supplier_quantity": 86.648534,
+                "delivery_delay": 1,
+                "defect_rate": 3.121492,
+                "quality_score": 63.463494,
+                "maintenance_hours": 9,
+                "downtime_percentage": 0.052343,
+                "inventory_turnover": 8.630515,
+                "stock_out_rate": 0.081322,
+                "worker_productivity": 85.042379,
+                "safety_incidents": 0,
+                "energy_consumption": 2419.616785,
+                "energy_efficiency": 0.468947,
+                "additive_process_time": 5.551639,
+                "additive_material_cost": 236.439301
+            }
         }
 
